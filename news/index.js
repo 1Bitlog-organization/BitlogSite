6b2a509c64f39b7818e18efceebbf6d968c0adb3
Thorgathis <nicolas.kopytin@gmail.com>
2023-05-30 22:01:10 +0300

    fix minor bugs, setting up news server

--- news/index.js
+++ news/index.js
@@ -1,4 +1,4 @@
-var CrmLink = "http://localhost:1337/api/news-api/";
+var CrmLink = "http://94.19.185.98:1337/api/news-api/";
 
 var getJSON = function (url, callback) {
     var xhr = new XMLHttpRequest();
@@ -20,7 +20,6 @@ var total;
 
 getJSON(CrmLink, function (err, data) {
     if (err !== null) {
-        alert("Something went wrong: " + err);
     } else {
         // dc = document.getElementById("display");
         // dc.innerHTML =
@@ -32,54 +31,56 @@ getJSON(CrmLink, function (err, data) {
                 if (err !== null) {
                     alert("Something went wrong: " + err);
                 } else {
-                    var Stp = data.data.attributes.publishedAt;
+                    if (data.data.attributes.DRAFT != true) {
+                        var Stp = data.data.attributes.publishedAt;
 
-                    var date =
-                        Stp.slice(0, 4) +
-                        "." +
-                        Stp.slice(5, 7) +
-                        "." +
-                        Stp.slice(8, 10);
-                    var time = Stp.slice(11, 13) + ":" + Stp.slice(14, 16);
-                    var datetime = date + " " + time;
+                        var date =
+                            Stp.slice(0, 4) +
+                            "." +
+                            Stp.slice(5, 7) +
+                            "." +
+                            Stp.slice(8, 10);
+                        var time = Stp.slice(11, 13) + ":" + Stp.slice(14, 16);
+                        var datetime = date + " " + time;
 
-                    pastePoint = document.querySelector("div.display");
+                        pastePoint = document.querySelector("div.display");
 
-                    mainElement = document.createElement("div");
-                    mainElement.classList.add(
-                        "text-white",
-                        "bg-dark",
-                        "mb-3",
-                        "crd"
-                    );
+                        mainElement = document.createElement("div");
+                        mainElement.classList.add(
+                            "text-white",
+                            "bg-dark",
+                            "mb-3",
+                            "crd"
+                        );
 
-                    dataElement = document.createElement("div");
-                    dataElement.classList.add("element");
-                    dataElement.innerHTML += `<div class="spacer" style="height: 1%;"></div>`;
-                    dataElement.innerHTML +=
-                        "<h4>" + data.data.attributes.Title + "</h4>";
-                    dataElement.innerHTML +=
-                        "<p>" + data.data.attributes.Text + "</p>";
-                    if (data.data.attributes.Forms) {
+                        dataElement = document.createElement("div");
+                        dataElement.classList.add("element");
+                        dataElement.innerHTML += `<div class="spacer" style="height: 1%;"></div>`;
                         dataElement.innerHTML +=
-                            "<a>" +
-                            data.data.attributes.publishedAt +
-                            "</a>" +
-                            `<a class="btn btn-default forms-bt" target="_blank" href="` +
-                            data.data.attributes.FormUrl +
-                            `">` +
-                            "Go To Yandex Forms" +
-                            "</a>";
-                    } else if (
-                        data.data.attributes.Forms ||
-                        data.data.attributes.Forms == null
-                    ) {
-                        dataElement.innerHTML += "<a>" + datetime + "</a>";
-                    }
+                            "<h4>" + data.data.attributes.Title + "</h4>";
+                        dataElement.innerHTML +=
+                            "<p>" + data.data.attributes.Text + "</p>";
+                        if (data.data.attributes.Forms == true) {
+                            dataElement.innerHTML +=
+                                "<a>" +
+                                data.data.attributes.publishedAt +
+                                "</a>" +
+                                `<a class="btn btn-default forms-bt" target="_blank" href="` +
+                                data.data.attributes.FormUrl +
+                                `">` +
+                                "Go To Yandex Forms" +
+                                "</a>";
+                        } else if (
+                            data.data.attributes.Forms == false ||
+                            data.data.attributes.Forms == null
+                        ) {
+                            dataElement.innerHTML += "<a>" + datetime + "</a>";
+                        }
 
-                    dataElement.innerHTML += `<div class="spacer" style="height: 20px;"></div>`;
-                    mainElement.insertBefore(dataElement, null);
-                    pastePoint.insertBefore(mainElement, null);
+                        dataElement.innerHTML += `<div class="spacer" style="height: 20px;"></div>`;
+                        mainElement.insertBefore(dataElement, null);
+                        pastePoint.insertBefore(mainElement, null);
+                    }
                 }
             });
         }
